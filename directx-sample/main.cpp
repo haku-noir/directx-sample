eb000cbad4fc6deb3e015a42c0787755990386ca
haku-noir <hakunoir.00@gmail.com>
2021-10-09 16:24:13 +0900

    add: feature to show empty window

--- directx-sample/main.cpp
+++ directx-sample/main.cpp
@@ -1,15 +1,23 @@
 #include <Windows.h>
+#include <tchar.h>
 #ifdef _DEBUG
     #include <iostream>
 #endif // _DEBUG
 
-void DebugOutputFormatString(const char* format, ...) {
-#ifdef _DEBUG
-    va_list valist;
-    va_start(valist, format);
-    printf(format, valist);
-    va_end(valist);
-#endif // _DEBUG
+#define WINDOW_CLASS _T("DX12Sample")
+#define WINDOW_TITLE _T("DX12ƒeƒXƒg")
+#define	WINDOW_STYLE WS_OVERLAPPEDWINDOW
+#define WINDOW_WIDTH 1280
+#define WINDOW_HEIGHT 720
+
+using namespace std;
+
+LRESULT WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+    if (msg == WM_DESTROY) {
+        PostQuitMessage(0);
+        return 0;
+    }
+    return DefWindowProc(hwnd, msg, wparam, lparam);
 }
 
 #ifdef _DEBUG
@@ -17,7 +25,45 @@ int main() {
 #else
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 #endif // _DEBUG
-    DebugOutputFormatString("Show window test.");
-    getchar();
+    WNDCLASSEX w = {};
+    w.cbSize = sizeof(WNDCLASSEX);
+    w.lpfnWndProc = (WNDPROC)WindowProcedure;
+    w.lpszClassName = WINDOW_CLASS;
+    w.hInstance = GetModuleHandle(nullptr);
+
+    RegisterClassEx(&w);
+
+    RECT wrc = { 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT };
+    AdjustWindowRect(&wrc, WINDOW_STYLE, false);
+
+    HWND hwnd = CreateWindow(
+        w.lpszClassName,
+        WINDOW_TITLE,
+        WINDOW_STYLE,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        wrc.right - wrc.left,
+        wrc.bottom - wrc.top,
+        nullptr,
+        nullptr,
+        w.hInstance,
+        nullptr
+    );
+    ShowWindow(hwnd, SW_SHOW);
+
+    MSG msg = {};
+
+    while (true) {
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+
+        if (msg.message == WM_QUIT) {
+            break;
+        }
+    }
+    UnregisterClass(w.lpszClassName, w.hInstance);
+
     return 0;
 }
