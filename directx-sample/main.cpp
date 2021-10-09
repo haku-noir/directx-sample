ebc1c4d9383db9d9ca183eadb42646f0d3844817
haku-noir <hakunoir.00@gmail.com>
2021-10-09 15:14:24 +0900

    add: feature to initialize d3d12

--- directx-sample/main.cpp
+++ directx-sample/main.cpp
@@ -1,8 +1,16 @@
 #include <Windows.h>
 #include <tchar.h>
+#include <vector>
 #ifdef _DEBUG
     #include <iostream>
 #endif // _DEBUG
+
+using namespace std;
+
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
 
 #define WINDOW_CLASS _T("DX12Sample")
 #define WINDOW_TITLE _T("DX12ƒeƒXƒg")
@@ -10,7 +18,16 @@
 #define WINDOW_WIDTH 1280
 #define WINDOW_HEIGHT 720
 
-using namespace std;
+ID3D12Device* _dev = nullptr;
+IDXGIFactory6* _dxgiFactory = nullptr;
+IDXGISwapChain4* _swapchain = nullptr;
+
+D3D_FEATURE_LEVEL levels[] = {
+    D3D_FEATURE_LEVEL_12_1,
+    D3D_FEATURE_LEVEL_12_0,
+    D3D_FEATURE_LEVEL_11_1,
+    D3D_FEATURE_LEVEL_11_0,
+};
 
 LRESULT WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
     if (msg == WM_DESTROY) {
@@ -49,6 +66,37 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
         w.hInstance,
         nullptr
     );
+
+    auto result = CreateDXGIFactory1(IID_PPV_ARGS(&_dxgiFactory));
+
+    std::vector<IDXGIAdapter*> adapters;
+    IDXGIAdapter* tmpAdapter = nullptr;
+    for(int i = 0; _dxgiFactory->EnumAdapters(i, &tmpAdapter) != DXGI_ERROR_NOT_FOUND; i++){
+        adapters.push_back(tmpAdapter);
+    }
+
+    for (auto adpt : adapters) {
+        DXGI_ADAPTER_DESC adesc = {};
+        adpt->GetDesc(&adesc);
+        std::wstring strDesc = adesc.Description;
+
+        if (strDesc.find(L"NVIDIA") != std::string::npos) {
+            tmpAdapter = adpt;
+            break;
+        }
+    }
+
+    D3D_FEATURE_LEVEL featureLevel;
+    for (auto lv : levels) {
+        if (D3D12CreateDevice(tmpAdapter, lv, IID_PPV_ARGS(&_dev)) == S_OK) {
+            featureLevel = lv;
+            break;
+        }
+    }
+    if (_dev == nullptr) {
+        return -1;
+    }
+
     ShowWindow(hwnd, SW_SHOW);
 
     MSG msg = {};
